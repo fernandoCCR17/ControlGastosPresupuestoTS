eb779950b970ed2370c7875e4c7e601ba295410f
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2025-08-11 22:09:02 -0600

    Creacion del reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import BudgetForm from "./components/BudgetForm"
+
 function App() {
   return (
     <>
@@ -8,7 +10,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <p>Formulario aquí</p>
+        <BudgetForm />
       </div>
     </>
   )
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,5 +1,40 @@
+import { useMemo, useState, type ChangeEvent } from "react"
+
+
 export default function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+  
+  function handleChange(e: ChangeEvent<HTMLInputElement>){
+    setBudget(e.target.valueAsNumber);
+  }
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget]);
+
   return (
-    <div>BudgetForm</div>
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Definir Presupuesto
+        </label>
+
+        <input 
+          id="budget"
+          type="number" 
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Define tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      
+      <input 
+        type="submit" 
+        value='Definir presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-10 disabled:cursor-auto"
+        disabled={isValid}
+      />
+    </form>
   )
 }
