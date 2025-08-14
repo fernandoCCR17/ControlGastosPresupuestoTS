750e3906dd93d6a79c697ff27a90b396eabdc4f3
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2025-08-13 22:32:09 -0600

    custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import BudgetForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
 
 function App() {
+  const { state, dispatch } = useBudget();
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
