293cbef29069a5491335ad18a8b25acf8dfa9f78
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2025-08-24 22:24:30 -0600

    Form Gastos

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,129 @@
+import { categories } from "../data/categories";
+import type { DraftExpense, Value } from "../types";
+import DatePicker from 'react-date-picker';
+import 'react-calendar/dist/Calendar.css'
+import 'react-date-picker/dist/DatePicker.css'
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import ErrorMessage from "./ErrorMessage";
+
 export default function ExpenseForm() {
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: "",
+    category: "",
+    date: new Date()
+  });
+  const [error, setError] = useState('');
+
+  const handleChangeDate = (value: Value) => {
+    setExpense({...expense, date: value})
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    const {name, value} = e.target;
+    const isAmountField = ['amount'].includes(name);
+
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? +value : value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //Validar
+    if(Object.values(expense).includes('')){
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+  }
+
   return (
-    <div>ExpenseForm</div>
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <legend
+        className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
+      >Nuevo Gasto</legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="expenseName"
+          className="text-xl"
+        >Nombre Gasto:</label>
+
+        <input 
+          type="text" 
+          id="expenseName"
+          placeholder="Añade el nombre del gasto: ej. comida"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+          value={expense.expenseName}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="amount"
+          className="text-xl"
+        >Cantidad:</label>
+
+        <input 
+          type="number" 
+          id="amount"
+          placeholder="Añade la cantidad del gasto: ej. 300"
+          className="bg-slate-100 p-2"
+          name="amount"
+          value={expense.amount}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="category"
+          className="text-xl"
+        >Cantidad:</label>
+
+        <select 
+          id="category"
+          className="bg-slate-100 p-2"
+          name="category"
+          value={expense.category}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione --</option>
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="date"
+          className="text-xl"
+        >Fecha Gasto:</label>
+
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+          onChange={handleChangeDate}
+        />
+      </div>
+
+      <input 
+        type="submit" 
+        value="Registrar Gasto" 
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase rounded-lg font-bold"
+      />
+    </form>
   )
 }
--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -5,7 +5,7 @@ import { useBudget } from '../hooks/useBudget'
 import ExpenseForm from './ExpenseForm';
 
 export default function ExpenseModal() {
-    const {state, dispatch} = useBudget();
+    const { state, dispatch } = useBudget();
 
     return (
         <>
@@ -14,41 +14,41 @@ export default function ExpenseModal() {
                     type="button"
                     onClick={() => dispatch({ type: 'show-modal' })}
                 >
-                <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full' />
+                    <PlusCircleIcon className='w-16 h-16 text-blue-600 rounded-full  cursor-pointer' />
                 </button>
             </div>
 
             <Transition appear show={state.modal} as={Fragment}>
                 <Dialog as="div" className="relative z-10" onClose={() => dispatch({ type: 'close-modal' })}>
-                <TransitionChild
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black bg-opacity-75" />
-                </TransitionChild>
-
-                <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center p-4 text-center">
                     <TransitionChild
                         as={Fragment}
                         enter="ease-out duration-300"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-75"
                         leave="ease-in duration-200"
-                        leaveFrom="opacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
+                        leaveFrom="opacity-75"
+                        leaveTo="opacity-0"
                     >
-                        <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                            <ExpenseForm />
-                        </DialogPanel>
+                        <div className="fixed inset-0 bg-black opacity-75"></div>
                     </TransitionChild>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <TransitionChild
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <DialogPanel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <ExpenseForm />
+                                </DialogPanel>
+                            </TransitionChild>
+                        </div>
                     </div>
-                </div>
                 </Dialog>
             </Transition>
         </>
